77d6b95143159480e5c25737aaee0675e2c02b04
PETER WAMBEDDE <wambexpeter@gmail.com>
2023-11-06 14:41:37 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function highlight() {
     //Write your code here
-
+    
+	let a=document.getElementsByTagName("strong");
+	for(let i=0;i<a.length;i++){
+		a[i].style.color="green";
+	}
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+   let a=document.getElementsByTagName("strong");
+	for(let i=0;i<a.length;i++){
+		a[i].style.color="black";
+}
     
 }
